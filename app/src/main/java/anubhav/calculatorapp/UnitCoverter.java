8ac9eb0392a800c293d0b3a6c11ebd6f3901e69d
Rajeev <RajeevPankaj.Shukla@iiitb.org>
2019-11-12 00:57:06 +0530

    Deleted UnitArea and UnitWeight

--- app/src/main/java/anubhav/calculatorapp/UnitCoverter.java
+++ app/src/main/java/anubhav/calculatorapp/UnitCoverter.java
@@ -24,18 +24,18 @@ public class UnitCoverter extends AppCompatActivity {
         Intent i;
         switch(v.getId())
         {
-            case R.id.area:
-                i=new Intent(this,UnitArea.class);
-                startActivity(i);
-                break;
+//            case R.id.area:
+//                i=new Intent(this,UnitArea.class);
+//                startActivity(i);
+//                break;
             case R.id.length:
                 i=new Intent(this,UnitLength.class);
                 startActivity(i);
                 break;
-            case R.id.weight:
-                i=new Intent(this,UnitWeight.class);
-                startActivity(i);
-                break;
+//            case R.id.weight:
+//                i=new Intent(this,UnitWeight.class);
+//                startActivity(i);
+//                break;
             case R.id.tempearture:
                 i=new Intent(this,UnitTemperature.class);
                 startActivity(i);
